cff4d1ce5065afcafcfbda5fda04f2b165ee1174
Zero Unix <zerounix32@gmail.com>
2018-02-22 16:49:07 +0700

    Update Example_1.cpp

--- Examples/CPP/Example_1.cpp
+++ Examples/CPP/Example_1.cpp
@@ -1,11 +1,9 @@
-// Example 1: Elevate current process if not during running 
+// Example 1: Elevate current process during runtime
 
 // ... Initializing stuffs
 
 int __cdecl main(int argc, wchar_t* argv[]) {
-  //This process is set to be run as asinvoker (non-elevate
-  int a = 5;
-  int b = a++;
+  //This process is set to be run as asinvoker (non-elevate)
   if(AdministratorPrivilege() == FALSE)
     ElevateCurrentProcess(); // At this point, your process will be elevated to the highest privilege
   return 0;
